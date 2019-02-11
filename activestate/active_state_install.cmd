b2b823e2503ed0b045abe0a0ce0d66c4172ea93b
Yurii Cherkasov <yuri.cherkasov.accounts@gmail.com>
2019-02-11 14:36:45 +0700

    ActiveState Perl and Python logs

--- activestate/active_state_install.cmd
+++ activestate/active_state_install.cmd
@@ -1,8 +1,10 @@
 @echo off
 
 REM ActivePerl
+echo Install ActivePerl 5.26.3
 start /wait ActivePerl-5.26.3.2603-MSWin32-x64-a95bce075.exe /quiet
 ppm install Text::Template
 
 REM ActivePython
+echo Install ActivePython 2.7.14
 start /wait ActivePython-2.7.14.2717-win64-x64-404905.exe /quiet
