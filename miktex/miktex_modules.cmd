6fb33028dab2699974df7097f8295d58a31099c0
Yurii Cherkasov <yuri.cherkasov.accounts@gmail.com>
2019-02-11 14:48:26 +0700

    echo off

--- miktex/miktex_modules.cmd
+++ miktex/miktex_modules.cmd
@@ -1,1 +1,2 @@
+@echo off
 mpm.exe --verbose --install=koma-script --install=ucs --install=tabulary --install=url --install=fancybox --install=fancyvrb --install=bera --install=charter --install=mptopdf
