0ad7f6203a8c99cd128d24cb7a254a65391bf20a
Yurii Cherkasov <yuri.cherkasov.accounts@gmail.com>
2019-02-11 14:40:29 +0700

    Silent install of Cygwin64

--- cygwin/install-x64.cmd
+++ cygwin/install-x64.cmd
@@ -1,7 +1,2 @@
-setup-x86_64.exe ^
---quiet-mode ^
---local-package-dir "%USERPROFILE%\Downloads\cygwin-packages" ^
---verbose ^
---site http://artfiles.org/cygwin.org/pub/cygwin/ ^
---root "%USERPROFILE%\cygwin64"
---packages openssh,git,rsync,nano
+@echo off
+setup-x86_64.exe --quiet-mode --local-package-dir "%USERPROFILE%\Downloads\cygwin-packages" --verbose --site http://artfiles.org/cygwin.org/pub/cygwin/ --root "%USERPROFILE%\cygwin64" --packages openssh,git,rsync,nano
