77c99cd809344f4fd84c0141b478cdb63d9b9ced
Yurii Cherkasov <yuri.cherkasov.accounts@gmail.com>
2019-02-11 14:47:57 +0700

    Install libvpx echo off

--- libvpx/compile-x64.cmd
+++ libvpx/compile-x64.cmd
@@ -1,2 +1,3 @@
+@echo off
 %USERPROFILE%\cygwin64\bin\dos2unix compile-x64.sh
 %USERPROFILE%\cygwin64\bin\bash compile-x64.sh
--- libvpx/compile-x86.cmd
+++ libvpx/compile-x86.cmd
@@ -1,2 +1,3 @@
+@echo off
 %USERPROFILE%\cygwin64\bin\dos2unix compile-x86.sh
 %USERPROFILE%\cygwin64\bin\bash compile-x86.sh
