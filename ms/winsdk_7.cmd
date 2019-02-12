ef3155a2a696752d09abc26165bc25783a77848c
Yurii Cherkasov <yurii.cherkasov@protonmail.com>
2019-02-12 20:22:37 +0700

    Split Windows SDKs 7 and 8 batch files

---
+++ ms/winsdk_7.cmd
@@ -0,0 +1,5 @@
+@echo off
+REM SDK 7
+echo Windows SDK 7
+start /wait setup.exe -q -params:ADDLOCAL=ALL
+
--- ms/winsdk_8.cmd
+++ ms/winsdk_8.cmd
@@ -1,6 +1,5 @@
-REM SDK 7.1
-start /wait setup.exe -q -params:ADDLOCAL=ALL
-
+@echo off
 REM Windows SDK 8.1
+echo Windows SDK 8.1
 start /wait sdksetup.exe /features + /quiet
 
