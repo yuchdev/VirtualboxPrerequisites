c0d5c4fb97619ffabead4dc711d1b99123d93ea8
Yurii Cherkasov <yurii.cherkasov@protonmail.com>
2019-03-31 13:58:44 +0700

    Fix perl PATH env

--- install_software/activestate/active_state_install.cmd
+++ install_software/activestate/active_state_install.cmd
@@ -5,10 +5,12 @@ cd active-state-perl-5.26.3-python-2.7.14\
 REM ActivePerl
 echo Install ActivePerl 5.26.3
 start /wait ActivePerl-5.26.3.2603-MSWin32-x64-a95bce075.exe /quiet
-C:\Perl64\bin\ppm.bat install Text::Template
+
 
 REM ActivePython
 echo Install ActivePython 2.7.14
 start /wait ActivePython-2.7.14.2717-win64-x64-404905.exe /quiet
+
+C:\Perl64\bin\ppm.bat install Text::Template
 
 cd ..
