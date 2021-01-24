f26ed4da54dbf9bc54fcf0a1d9cbf6a995be768e
Yurii Cherkasov <yurii.cherkasov@protonmail.com>
2021-01-25 01:50:53 +0700

    Vbox env script - make system-wide

--- install_software/vbox_env.cmd
+++ install_software/vbox_env.cmd
@@ -1,17 +1,17 @@
 @ehco off
 
 echo "Set Virtualbox release logging flags"
-setx VBOXSVC_RELEASE_LOG "main.e.l.f+gui.e.l.f"
-setx VBOXSVC_RELEASE_LOG_FLAGS "time tid thread"
+setx /m VBOXSVC_RELEASE_LOG "main.e.l.f+gui.e.l.f"
+setx /m VBOXSVC_RELEASE_LOG_FLAGS "time tid thread"
 
 echo "Set Virtualbox release logging directory"
-setx VBOXSVC_RELEASE_LOG_DEST "C:\Users\atatat\.VirtualBox"
+setx /m VBOXSVC_RELEASE_LOG_DEST "C:\Users\atatat\.VirtualBox"
 
 echo "Set Virtualbox debug logging flags"
-setx VBOX_LOG "main.e.l.f+gui.e.l.f"
-setx VBOX_LOG_FLAGS "time tid thread"
+setx /m VBOX_LOG "main.e.l.f+gui.e.l.f"
+setx /m VBOX_LOG_FLAGS "time tid thread"
 
 echo "Set Virtualbox debug logging directory"
-setx VBOX_LOG_DEST "C:\Users\atatat\.VirtualBox"
-setx WATCOM "C:\WATCOM"
-setx WHTMLHELP "C:\WATCOM\BINNT\HELP"
+setx /m VBOX_LOG_DEST "C:\Users\atatat\.VirtualBox"
+setx /m WATCOM "C:\WATCOM"
+setx /m WHTMLHELP "C:\WATCOM\BINNT\HELP"
